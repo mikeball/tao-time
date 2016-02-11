5689486c838914d03f3b9d9d3f504f95812e92c7
Mike Ball <michaelhball@gmail.com>
2016-02-10 21:46:46 -0800

    Add project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.taoclj/time "0.1.0"
-  :description "Basic time functions"
-  :url "http://example.com/FIXME"
+  :description "Basic time functions in readable form."
+  :url "https://github.com/mikeball/tao-time"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
